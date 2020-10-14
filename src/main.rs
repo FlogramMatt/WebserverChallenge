7cb33deca13722cf8bd104dbdfe9a3b7baab13ee
Matt Czarnek <matt@frink.global>
2020-10-13 22:02:20 -0400

    Successfully calling a function upon Get apt/v1/users being called

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,17 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("apt" / "v1" / "users" / String)
-        .map(|name| format!("Hello, {}!", name));
+    let hello = warp::path!("apt" / "v1" / "users" / String).and_then(get_user);
+        //.method("GET");
+        //.map(|name| format!("Hello, {}!", name));
 
     warp::serve(hello)
         .run(([127, 0, 0, 1], 3030))
         .await;
+}
+
+pub async fn get_user(name: String) -> Result<impl warp::Reply, warp::Rejection> {
+    let reply = format!("Hello, {}!", name);
+     println!("{}", &reply);
+    Ok(warp::reply::html(reply))
 }
