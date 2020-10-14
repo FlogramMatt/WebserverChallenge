c95cc990b206bd678e29645bf77126e00170cf93
Matt Czarnek <matt@frink.global>
2020-10-14 12:43:27 -0400

    Step one works, additionally can query for an individual user instead of all users by appending user id

--- src/handlers.rs
+++ src/handlers.rs
@@ -16,17 +16,12 @@ pub async fn list_users() -> Result<impl warp::Reply, warp::Rejection> {
     //get users from https://jsonplaceholder.typicode.com/users
 
     //for a real project I would use nonblocking and handle this asynchronously
-    let mut res = reqwest::get("https://jsonplaceholder.typicode.com/users").await.unwrap();
+    //also I wouldn't use unwrap which might panic and crash the entire server
+    let res = reqwest::get("https://jsonplaceholder.typicode.com/users").await.unwrap();
 
     let body = res.text().await.unwrap();
 
-    //temporarily printing to string
-    println!("Body:\n{}",body);
-
-    //let users: Vec<User> = Users.clone();
-    //Ok(warp::reply::json(&users))
-    let our_ids = vec![1, 3, 7, 13]; //random vector being returned for now
-    Ok(warp::reply::json(&our_ids))
+    Ok(warp::reply::with_status(body,StatusCode::OK))
 }
 
 /// Gets a single user from the data store
@@ -36,18 +31,32 @@ pub async fn list_users() -> Result<impl warp::Reply, warp::Rejection> {
 /// # Arguments
 ///
 /// * `guid` - String -> the id of the user to retrieve
-pub async fn get_user(user_id: i32) -> Result<Box<dyn warp::Reply>, warp::Rejection> {
+pub async fn get_user(user_id: i32) -> Result<impl warp::Reply, warp::Rejection> {
     //get users from https://jsonplaceholder.typicode.com/users
 
-    /*    for user in users.iter() {
-            if customer.guid == guid {
-                return Ok(Box::new(warp::reply::json(&user)));
-            }
-        }*/
+    let mut url:String = "https://jsonplaceholder.typicode.com/users/".to_string();
+    url.push_str(user_id.to_string().as_str());
 
-    let mut res = reqwest::blocking::get("https://jsonplaceholder.typicode.com/users").unwrap();
+    let res = reqwest::get(url.as_str()).await.unwrap();
+    let body = res.text().await.unwrap();
 
-    Ok(Box::new(StatusCode::NOT_FOUND))
+    if(body == "{}"){
+        return Ok(warp::reply::with_status("Not Found".to_string(), StatusCode::NOT_FOUND));
+    }
+
+    Ok(warp::reply::with_status(body, StatusCode::OK))
+    /*let users: Vec<User> = serde_json::from_str(body.as_str()).unwrap();
+
+    for user in users.iter()
+    {
+        if(user.id == user_id){
+            let user_string = serde_json::to_string(user).unwrap();
+
+            Ok(warp::reply::with_status(user_string, StatusCode::OK))
+        }
+    }*/
+
+    //Ok(Box::new(StatusCode::NOT_FOUND))
 }
 
 /*/// Creates a new customer
