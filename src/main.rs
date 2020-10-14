96e8f9a72b0f272fba08d78dc5388711f928f5d9
Matt Czarnek <matt@frink.global>
2020-10-14 16:47:04 -0400

    Cleaned the code a little bit.

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,10 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main(){
+    //define the paths that will be exposed by the warp REST API and functions that will run when executed
     let user_routes = routes::user_routes();
 
+    //Start the server running at localhost:3000
     warp::serve(user_routes)
         .run(([127, 0, 0, 1], 3000))
         .await;
